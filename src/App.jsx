cfbefac1d3bcfe644083558d0da6dbc73b368ad3
MyTxweProgrammit <humanyoungtee@gmail.com>
2025-08-09 21:42:50 +0700

    WakaTime Test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className='border-test w-screen h-screen'>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
